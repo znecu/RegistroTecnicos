5bbf1dcb83489507c2e81f38c018485625cef222
znecu <alma_maria@ucne.edu.do>
2024-08-28 23:18:51 -0400

    Prueba modelo

---
+++ RegistroTecnicos/RegistroTecnicos/Models/Tecnicos.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models;
+
+public class Tecnicos
+{
+    [Key]
+    public int TecnicoId { get; set; }
+    [Required]
+
+    public string NombreTecnico { get; set; }
+    [Required]
+
+    public decimal SueldoHora { get; set; }
+}
