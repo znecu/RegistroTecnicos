431a751866a3dad78c41766a4164c32315d4ec5f
znecu <alma_maria@ucne.edu.do>
2024-08-28 23:54:52 -0400

    Creé cree la carpeta models y agregué el modelo Tecnicos.

--- RegistroTecnicos/RegistroTecnicos/Models/Tecnicos.cs
+++ RegistroTecnicos/RegistroTecnicos/Models/Tecnicos.cs
@@ -6,10 +6,14 @@ public class Tecnicos
 {
     [Key]
     public int TecnicoId { get; set; }
-    [Required]
 
-    public string NombreTecnico { get; set; }
-    [Required]
+    [Required(ErrorMessage = "Este campo obligatorio. ")]
+    [RegularExpression(@"^[a-zA-z]+$", ErrorMessage = "En este campo solo se permiten letras. ")]
+    public string? NombreTecnico { get; set; }
+
+
+    [Required(ErrorMessage = "Este campo es obligatorio. ")]
+    [Range(minimum: 0.1, maximum: 500000, ErrorMessage = "Por favor, ingrese una cantidad mayor a 0 y menor o igual a $500,000. ")]
 
     public decimal SueldoHora { get; set; }
 }
