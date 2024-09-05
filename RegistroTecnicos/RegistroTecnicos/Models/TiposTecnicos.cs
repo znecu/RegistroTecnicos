2fa501f6f7261aef7f45b8359352919f1a076800
znecu <alma_maria@ucne.edu.do>
2024-09-04 23:32:05 -0400

    Creé el modelo TiposTecnicos

---
+++ RegistroTecnicos/RegistroTecnicos/Models/TiposTecnicos.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models;
+
+public class TiposTecnicos
+{
+    [Key]
+    public int TiposTecnicosId { get; set; }
+
+    [Required(ErrorMessage = "Este campo es obligatorio")]
+    [RegularExpression(@"^[a-zA-Z]\s", ErrorMessage = "Este campo solo permite letras. ")]
+    public string? Descripcion { get; set; }
+
+
+}
