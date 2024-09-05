e67cb76424cbb45daec073cd392aa73aab832332
znecu <alma_maria@ucne.edu.do>
2024-09-04 23:32:43 -0400

    Agregué TiposTecnicos al contexto

--- RegistroTecnicos/RegistroTecnicos/DAL/Contexto.cs
+++ RegistroTecnicos/RegistroTecnicos/DAL/Contexto.cs
@@ -8,4 +8,6 @@ public class Contexto : DbContext
     public Contexto(DbContextOptions<Contexto> options) : base(options) { }
 
     public DbSet<Tecnicos> Tecnicos { get; set; }
+
+    public DbSet<TiposTecnicos> TiposTecnicos { get; set; }
 }
