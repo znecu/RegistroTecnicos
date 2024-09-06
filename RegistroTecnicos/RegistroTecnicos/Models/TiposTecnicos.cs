26a23e5c251955940544a0be954ab7d51cd6daff
znecu <alma_maria@ucne.edu.do>
2024-09-05 20:03:01 -0400

    Modifiqué la RegularExpression

--- RegistroTecnicos/RegistroTecnicos/Models/TiposTecnicos.cs
+++ RegistroTecnicos/RegistroTecnicos/Models/TiposTecnicos.cs
@@ -8,7 +8,7 @@ public class TiposTecnicos
     public int TiposTecnicosId { get; set; }
 
     [Required(ErrorMessage = "Este campo es obligatorio")]
-    [RegularExpression(@"^[a-zA-Z]\s", ErrorMessage = "Este campo solo permite letras. ")]
+    [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Este campo solo permite letras. ")]
     public string? Descripcion { get; set; }
 
 
