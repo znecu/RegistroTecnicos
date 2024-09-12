55c4fcf5f784ef4030527628f901cfc92f96dacb
znecu <alma_maria@ucne.edu.do>
2024-09-11 22:23:04 -0400

    Creé el modelo de clientes

---
+++ RegistroTecnicos/RegistroTecnicos/Models/Clientes.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models;
+
+public class Clientes
+{
+    [Key]
+    public int ClientesId { get; set; }
+
+    [Required(ErrorMessage = "Este campo es obligatorio.")]
+    [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "En este campo solo se permiten letras. ")]
+    
+    public string? Nombres { get; set; }
+
+    [Required(ErrorMessage = "Este campo es obligatorio. ")]
+    [StringLength(10, ErrorMessage = "Ingrese un número de telefono válido. ")]
+    [RegularExpression(@"^\d+$", ErrorMessage = "Solo se permiten números.")]
+    public string? Whatsapp {  get; set; }
+}
