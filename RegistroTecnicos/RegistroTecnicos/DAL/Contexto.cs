c7c24efe730ea36a3615461c5c773d03e54d098d
znecu <alma_maria@ucne.edu.do>
2024-09-13 02:55:34 -0400

    Agregué trabajos al contexto

--- RegistroTecnicos/RegistroTecnicos/DAL/Contexto.cs
+++ RegistroTecnicos/RegistroTecnicos/DAL/Contexto.cs
@@ -10,5 +10,8 @@ public class Contexto : DbContext
     public DbSet<Tecnicos> Tecnicos { get; set; }
 
     public DbSet<TiposTecnicos> TiposTecnicos { get; set; }
+
     public DbSet<Clientes> Clientes { get; set; }
+
+    public DbSet<Trabajos> Trabajos { get; set; }
 }
