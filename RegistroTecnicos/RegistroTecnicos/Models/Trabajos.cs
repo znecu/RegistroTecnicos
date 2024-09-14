ec46079b625c93838d8eba1f97fea802914b7ac9
znecu <alma_maria@ucne.edu.do>
2024-09-14 02:48:08 -0400

    En el monto agregué una expresión regular con un rango y el ErrorMessage

--- RegistroTecnicos/RegistroTecnicos/Models/Trabajos.cs
+++ RegistroTecnicos/RegistroTecnicos/Models/Trabajos.cs
@@ -13,16 +13,21 @@ public class Trabajos
     public string? Descripcion { get; set; }
 
     [Required(ErrorMessage ="Este campo es obligatorio. ")]
+    [RegularExpression(@"^\d+(\.\d{1,2})?$", ErrorMessage = "Este campo solo permite números.")]
+    [Range(minimum: 0.1, maximum: 500000, ErrorMessage = "Por favor, ingrese una cantidad mayor a 0 y menor o igual a $500,000. ")]
     public decimal Monto { get; set; }
+
 
     public DateTime Fecha { get; set; }
 
+    //fk
     public int ClienteId { get; set; }
     [ForeignKey("ClienteId")]
-    public Clientes? Clientes { get; set; }
 
+    public Clientes? Clientes { get; set; }
 
     public int TecnicoId { get; set; }
     [ForeignKey("TecnicoId")]
+
     public Tecnicos? Tecnicos { get; set; }
 }
