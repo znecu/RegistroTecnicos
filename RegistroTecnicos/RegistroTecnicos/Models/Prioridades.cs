7e7edf936ff0b4e6085ea339da1b8539df9ecbb0
znecu <alma_maria@ucne.edu.do>
2024-09-17 16:09:59 -0400

    Creé el modelo de prioridades

---
+++ RegistroTecnicos/RegistroTecnicos/Models/Prioridades.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models;
+
+public class Prioridades
+{
+    [Key]
+    public int PrioridadId { get; set; }
+
+
+    [Required(ErrorMessage = "Este campo es obligatorio. ")]
+    [RegularExpression(@"^[a-zA-Z]\s+$,", ErrorMessage = "Este campo solo permite letras.")]
+    public string? Descripcion { set; get; }
+
+
+    [Required]
+    [RegularExpression(@"^\d+(\.\d{1,2})?$", ErrorMessage = "Este campo solo permite números.")]
+    public decimal Tiempo { get; set; }
+}
