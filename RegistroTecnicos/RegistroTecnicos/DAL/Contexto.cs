fcd73dfa7e5c4f1ec4f84417bd24aff3d747a002
znecu <alma_maria@ucne.edu.do>
2024-09-17 16:10:51 -0400

    Agregué prioridades al contexto

--- RegistroTecnicos/RegistroTecnicos/DAL/Contexto.cs
+++ RegistroTecnicos/RegistroTecnicos/DAL/Contexto.cs
@@ -14,4 +14,5 @@ public class Contexto : DbContext
     public DbSet<Clientes> Clientes { get; set; }
 
     public DbSet<Trabajos> Trabajos { get; set; }
+    public DbSet<Prioridades> Prioridades { get; set; }
 }
