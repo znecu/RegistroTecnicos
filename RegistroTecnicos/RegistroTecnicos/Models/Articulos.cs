47d76161d5ceee02853d4aff820c14955b984393
znecu <alma_maria@ucne.edu.do>
2024-10-16 19:38:33 -0400

    Creé el modelo de Articulos

---
+++ RegistroTecnicos/RegistroTecnicos/Models/Articulos.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RegistroTecnicos.Models;
+
+public class Articulos
+{
+    [Key]
+    public int ArticuloId { get; set; }
+
+    [Required(ErrorMessage = "Este campo es obligatorio. ")]
+    [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Este campo solo permite letras.")]
+    public string? Descripcion { get; set; }
+
+    [Required(ErrorMessage = "Este campo es obligatorio. ")]
+    [RegularExpression(@"^\d+(\.\d{1,2})?$", ErrorMessage = "Este campo solo permite números.")]
+    [Range(minimum: 0.1, maximum: 5000000, ErrorMessage = "Por favor, ingrese una cantidad mayor a 0 y menor o igual a $500,000. ")]
+    public decimal Costo { get; set; }
+
+    [Required(ErrorMessage = "Este campo es obligatorio. ")]
+    [RegularExpression(@"^\d+(\.\d{1,2})?$", ErrorMessage = "Este campo solo permite números.")]
+    [Range(minimum: 0.1, maximum: 500000, ErrorMessage = "Por favor, ingrese una cantidad mayor a 0 y menor o igual a $500,000. ")]
+    public decimal Precio { get; set; }
+
+    [Required]
+    public int Existencia { get; set; }
+
+ 
+}
