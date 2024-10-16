2e396133f657fcb2a4d54be6be554fbe4c7b1f8c
znecu <alma_maria@ucne.edu.do>
2024-10-16 19:36:36 -0400

    Migraciones

--- RegistroTecnicos/RegistroTecnicos/Migrations/20241016215155_Inicial.cs
+++ RegistroTecnicos/RegistroTecnicos/Migrations/20241016215155_Inicial.cs
@@ -3,14 +3,32 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
 namespace RegistroTecnicos.Migrations
 {
     /// <inheritdoc />
-    public partial class Connection : Migration
+    public partial class Inicial : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.CreateTable(
+                name: "Articulos",
+                columns: table => new
+                {
+                    ArticuloId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Descripcion = table.Column<string>(type: "TEXT", nullable: false),
+                    Costo = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Precio = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Existencia = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Articulos", x => x.ArticuloId);
+                });
+
             migrationBuilder.CreateTable(
                 name: "Clientes",
                 columns: table => new
@@ -109,10 +127,63 @@ namespace RegistroTecnicos.Migrations
                         onDelete: ReferentialAction.Cascade);
                 });
 
+            migrationBuilder.CreateTable(
+                name: "trabajoDetalles",
+                columns: table => new
+                {
+                    DetalleId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Cantidad = table.Column<int>(type: "INTEGER", nullable: false),
+                    Costo = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Precio = table.Column<decimal>(type: "TEXT", nullable: false),
+                    ArticuloId = table.Column<int>(type: "INTEGER", nullable: false),
+                    TrabajoId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_trabajoDetalles", x => x.DetalleId);
+                    table.ForeignKey(
+                        name: "FK_trabajoDetalles_Articulos_ArticuloId",
+                        column: x => x.ArticuloId,
+                        principalTable: "Articulos",
+                        principalColumn: "ArticuloId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_trabajoDetalles_Trabajos_TrabajoId",
+                        column: x => x.TrabajoId,
+                        principalTable: "Trabajos",
+                        principalColumn: "TrabajoId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Articulos",
+                columns: new[] { "ArticuloId", "Costo", "Descripcion", "Existencia", "Precio" },
+                values: new object[,]
+                {
+                    { 1, 900m, "Cable RJ45", 50, 1500m },
+                    { 2, 3000m, "Papel de impresora", 100, 100m },
+                    { 3, 3500m, "Pin de carga", 45, 5000m },
+                    { 4, 9000m, "Cámara de vigilancia", 50, 11000m },
+                    { 5, 2000m, "Router", 150, 3000m },
+                    { 6, 6500m, "Pantalla de celular", 100, 9000m },
+                    { 7, 4000m, "Tintas de impresoras", 200, 6000m }
+                });
+
             migrationBuilder.CreateIndex(
                 name: "IX_Tecnicos_TiposTecnicosId",
                 table: "Tecnicos",
                 column: "TiposTecnicosId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_trabajoDetalles_ArticuloId",
+                table: "trabajoDetalles",
+                column: "ArticuloId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_trabajoDetalles_TrabajoId",
+                table: "trabajoDetalles",
+                column: "TrabajoId");
 
             migrationBuilder.CreateIndex(
                 name: "IX_Trabajos_ClienteId",
@@ -133,6 +204,12 @@ namespace RegistroTecnicos.Migrations
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.DropTable(
+                name: "trabajoDetalles");
+
+            migrationBuilder.DropTable(
+                name: "Articulos");
+
             migrationBuilder.DropTable(
                 name: "Trabajos");
 
--- RegistroTecnicos/RegistroTecnicos/Migrations/ContextoModelSnapshot.cs
+++ RegistroTecnicos/RegistroTecnicos/Migrations/ContextoModelSnapshot.cs
@@ -17,6 +17,88 @@ namespace RegistroTecnicos.Migrations
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.8");
 
+            modelBuilder.Entity("RegistroTecnicos.Models.Articulos", b =>
+                {
+                    b.Property<int>("ArticuloId")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("INTEGER");
+
+                    b.Property<decimal>("Costo")
+                        .HasColumnType("TEXT");
+
+                    b.Property<string>("Descripcion")
+                        .IsRequired()
+                        .HasColumnType("TEXT");
+
+                    b.Property<int>("Existencia")
+                        .HasColumnType("INTEGER");
+
+                    b.Property<decimal>("Precio")
+                        .HasColumnType("TEXT");
+
+                    b.HasKey("ArticuloId");
+
+                    b.ToTable("Articulos");
+
+                    b.HasData(
+                        new
+                        {
+                            ArticuloId = 1,
+                            Costo = 900m,
+                            Descripcion = "Cable RJ45",
+                            Existencia = 50,
+                            Precio = 1500m
+                        },
+                        new
+                        {
+                            ArticuloId = 2,
+                            Costo = 3000m,
+                            Descripcion = "Papel de impresora",
+                            Existencia = 100,
+                            Precio = 100m
+                        },
+                        new
+                        {
+                            ArticuloId = 3,
+                            Costo = 3500m,
+                            Descripcion = "Pin de carga",
+                            Existencia = 45,
+                            Precio = 5000m
+                        },
+                        new
+                        {
+                            ArticuloId = 4,
+                            Costo = 9000m,
+                            Descripcion = "Cámara de vigilancia",
+                            Existencia = 50,
+                            Precio = 11000m
+                        },
+                        new
+                        {
+                            ArticuloId = 5,
+                            Costo = 2000m,
+                            Descripcion = "Router",
+                            Existencia = 150,
+                            Precio = 3000m
+                        },
+                        new
+                        {
+                            ArticuloId = 6,
+                            Costo = 6500m,
+                            Descripcion = "Pantalla de celular",
+                            Existencia = 100,
+                            Precio = 9000m
+                        },
+                        new
+                        {
+                            ArticuloId = 7,
+                            Costo = 4000m,
+                            Descripcion = "Tintas de impresoras",
+                            Existencia = 200,
+                            Precio = 6000m
+                        });
+                });
+
             modelBuilder.Entity("RegistroTecnicos.Models.Clientes", b =>
                 {
                     b.Property<int>("ClientesId")
@@ -93,6 +175,36 @@ namespace RegistroTecnicos.Migrations
                     b.ToTable("TiposTecnicos");
                 });
 
+            modelBuilder.Entity("RegistroTecnicos.Models.TrabajoDetalle", b =>
+                {
+                    b.Property<int>("DetalleId")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("INTEGER");
+
+                    b.Property<int>("ArticuloId")
+                        .HasColumnType("INTEGER");
+
+                    b.Property<int>("Cantidad")
+                        .HasColumnType("INTEGER");
+
+                    b.Property<decimal>("Costo")
+                        .HasColumnType("TEXT");
+
+                    b.Property<decimal>("Precio")
+                        .HasColumnType("TEXT");
+
+                    b.Property<int>("TrabajoId")
+                        .HasColumnType("INTEGER");
+
+                    b.HasKey("DetalleId");
+
+                    b.HasIndex("ArticuloId");
+
+                    b.HasIndex("TrabajoId");
+
+                    b.ToTable("trabajoDetalles");
+                });
+
             modelBuilder.Entity("RegistroTecnicos.Models.Trabajos", b =>
                 {
                     b.Property<int>("TrabajoId")
@@ -140,6 +252,25 @@ namespace RegistroTecnicos.Migrations
                     b.Navigation("TiposTecnicos");
                 });
 
+            modelBuilder.Entity("RegistroTecnicos.Models.TrabajoDetalle", b =>
+                {
+                    b.HasOne("RegistroTecnicos.Models.Articulos", "Articulos")
+                        .WithMany()
+                        .HasForeignKey("ArticuloId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.HasOne("RegistroTecnicos.Models.Trabajos", "Trabajos")
+                        .WithMany("TrabajoDetalle")
+                        .HasForeignKey("TrabajoId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.Navigation("Articulos");
+
+                    b.Navigation("Trabajos");
+                });
+
             modelBuilder.Entity("RegistroTecnicos.Models.Trabajos", b =>
                 {
                     b.HasOne("RegistroTecnicos.Models.Clientes", "Clientes")
@@ -165,6 +296,11 @@ namespace RegistroTecnicos.Migrations
                     b.Navigation("Prioridades");
 
                     b.Navigation("Tecnicos");
+                });
+
+            modelBuilder.Entity("RegistroTecnicos.Models.Trabajos", b =>
+                {
+                    b.Navigation("TrabajoDetalle");
                 });
 #pragma warning restore 612, 618
         }
