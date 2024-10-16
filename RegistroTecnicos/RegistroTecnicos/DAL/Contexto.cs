4ee966b67d24c9bf77d0cad1dd56139b6a9a00e5
znecu <alma_maria@ucne.edu.do>
2024-10-16 19:37:09 -0400

    Agregué Articulos en el OnModelCreating y a agregué los datos pertinentes

--- RegistroTecnicos/RegistroTecnicos/DAL/Contexto.cs
+++ RegistroTecnicos/RegistroTecnicos/DAL/Contexto.cs
@@ -15,4 +15,79 @@ public class Contexto : DbContext
 
     public DbSet<Trabajos> Trabajos { get; set; }
     public DbSet<Prioridades> Prioridades { get; set; }
+    public DbSet<TrabajoDetalle> trabajoDetalles { get; set; }
+
+    public DbSet<Articulos> Articulos { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Articulos>().HasData(
+            new List<Articulos>()
+            {
+                new()
+                {
+                    ArticuloId = 1,
+                    Descripcion = "Cable RJ45",
+                    Costo = 900,
+                    Precio = 1500,
+                    Existencia = 50,
+
+                },
+
+                new() {
+
+                    ArticuloId = 2,
+                    Descripcion = "Papel de impresora",
+                    Costo = 3000,
+                    Precio = 100,
+                    Existencia = 100,
+
+                },
+
+                new()
+                {
+                    ArticuloId = 3,
+                    Descripcion = "Pin de carga",
+                    Costo = 3500,
+                    Precio = 5000,
+                    Existencia = 45,
+
+                },
+
+                new() {
+                    ArticuloId = 4,
+                    Descripcion = "Cámara de vigilancia",
+                    Costo = 9000,
+                    Precio = 11000,
+                    Existencia = 50,
+                },
+
+                new() {
+                    ArticuloId = 5,
+                    Descripcion = "Router",
+                    Costo = 2000,
+                    Precio = 3000,
+                    Existencia = 150,
+                },
+
+                new() {
+                    ArticuloId = 6,
+                    Descripcion = "Pantalla de celular",
+                    Costo = 6500,
+                    Precio = 9000,
+                    Existencia = 100,
+                },
+                new() {
+                    ArticuloId = 7,
+                    Descripcion = "Tintas de impresoras",
+                    Costo = 4000,
+                    Precio = 6000,
+                    Existencia = 200,
+                }
+            }
+
+         );
+        base.OnModelCreating(modelBuilder);
+
+    }
 }
