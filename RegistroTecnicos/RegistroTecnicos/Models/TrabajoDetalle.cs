7ef3dc37846fa21ca1c60698d41388e8ba027830
znecu <alma_maria@ucne.edu.do>
2024-10-16 19:38:10 -0400

    Creé el modelo de TrabajoDetalle

---
+++ RegistroTecnicos/RegistroTecnicos/Models/TrabajoDetalle.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RegistroTecnicos.Models;
+
+public class TrabajoDetalle
+{
+    [Key]
+    public int DetalleId { get; set; }
+
+    [Required]
+    public int Cantidad { get; set; }
+
+    public decimal Costo { get; set; }
+
+    [Required(ErrorMessage = "Este campo es obligatorio. ")]
+    [RegularExpression(@"^\d+(\.\d{1,2})?$", ErrorMessage = "Este campo solo permite números.")]
+    [Range(minimum: 0.1, maximum: 500000, ErrorMessage = "Por favor, ingrese una cantidad mayor a 0 y menor o igual a $500,000. ")]
+    public decimal Precio { get; set; }
+
+    //fk
+    public int ArticuloId { get; set; }
+    [ForeignKey("ArticuloId")]
+    public Articulos? Articulos { get; set; }
+
+    public int TrabajoId { get; set; }
+    [ForeignKey("TrabajoId")]
+    public Trabajos? Trabajos { get; set; }
+
+}
