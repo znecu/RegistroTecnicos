8a892372d60e244078ddf7544c1bd8782e972469
znecu <alma_maria@ucne.edu.do>
2024-10-23 18:22:56 -0400

    Creé el modelo de CotizacionesDetalle

---
+++ RegistroTecnicos/RegistroTecnicos/Models/CotizacionesDetalle.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RegistroTecnicos.Models;
+
+public class CotizacionesDetalle
+{
+    [Key]
+    public int DetalleId { get; set; }
+
+    [Required(ErrorMessage = "Este campo es obligatorio. ")]
+    [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Este campo solo permite letras. ")]
+    public string? Cantidad { get; set; }
+
+    [Required(ErrorMessage = "Este campo es obligatorio. ")]
+    [RegularExpression(@"^\d+(\.\d{1,2})?$", ErrorMessage = "Este campo solo permite números.")]
+    [Range(minimum: 0.1, maximum: 500000, ErrorMessage = "Por favor, ingrese una cantidad mayor a 0 y menor o igual a $500,000. ")]
+    public double Precio { get; set; }
+
+    public int CotizacionId { get; set; }
+    [ForeignKey("CotizacionId")]
+    public Cotizaciones? Cotizaciones { get; set; }
+
+    public int ArticuloId { get; set; }
+    [ForeignKey("ArticuloId")]
+    public Articulos? Articulos { get; set; }
+
+}
